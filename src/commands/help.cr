ac432815cc2a66ff3dfe783d78ebb606555d3d43
HECTOR Guillaume <LeChatErrant@localhost.localdomain>
2019-04-25 20:08:38 +0200

    help improved; playing with string.build

--- src/commands/help.cr
+++ src/commands/help.cr
@@ -1,8 +1,11 @@
+require "../Commands.cr"
+
 def help(user, args)
-  user.reply(214, "The following commands are recognized:\n"\
-  "ABOR ACCT ALLO APPE CDUP CWD  DELE EPRT EPSV FEAT HELP LIST MDTM MKD\n"\
-  "MODE NLST NOOP OPTS PASS PASV PORT PWD  QUIT REIN REST RETR RMD  RNFR\n"\
-  "RNTO SITE SIZE SMNT STAT STOR STOU STRU SYST TYPE USER XCUP XCWD XMKD\n"\
-  "XPWD XRMD")
+  str = String.build do |io|
+    io << "The following commands are recongnized:\n"
+    Commands::COMMANDS.each {|key, _| io << key << " "}
+  end
+  str = str.rstrip
+  user.reply(214, str)
   user.reply(214, "Help OK.")
 end
