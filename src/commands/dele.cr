0dd84a55c79d5aca8c9a85be19fa4b70d9f1f85e
HECTOR Guillaume <LeChatErrant@guigui.home>
2019-04-28 16:32:22 +0200

    rm_r using FileUtils

--- src/commands/dele.cr
+++ src/commands/dele.cr
@@ -1,20 +1,10 @@
-private def rm_r(path : String) : Nil
-  if Dir.exists?(path) && !File.symlink?(path)
-    Dir.each_child(path) do |entry|
-      src = File.join(path, entry)
-      rm_r(src)
-    end
-    Dir.rmdir(path)
-  else
-    File.delete(path)
-  end
-end
+require "file_utils"
 
 def dele(user, args)
   return user.reply(550, "Failed to delete file.") if args.size != 1
   path = File.expand_path(args[0], user.working_directory)
   return user.reply(550, "Failed to delete file.") if !File.exists? args[0]
-  rm_r path
+  FileUtils.rm_r path
   user.reply(250, "File successfully deleted.")
 rescue e
   puts e.message
