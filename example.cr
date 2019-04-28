4f383c50f11e3df1b35e0a8e0f3d340c35e9c05a
HECTOR Guillaume <LeChatErrant@guigui.home>
2019-04-28 17:28:50 +0200

    updated example

--- example.cr
+++ example.cr
@@ -1,15 +1,15 @@
 require "./src/CrystalFTP.cr"
 
 if ARGV.size != 2
-    STDERR.puts "Usage:\n\t./crystalFTP port root"
-    exit 84
+  STDERR.puts "Usage:\n\t./crystalFTP port root_directory"
+  exit 84
 end
 
 include CrystalFTP
 
 port, root = ARGV
 
-puts FTPServer::VERSION
+puts "Using version #{FTPServer::VERSION}"
 server = FTPServer.new(port: port.to_i, root: root)
 server.start
 sleep
